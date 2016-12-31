51bac2b7e94dfb8f0efb4a30e15949926ccbb0be
mk7087 <yamk4054@outlook.com>
2016-12-31 16:06:16 +0900

    Added Visual Studio Project

---
+++ Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Windows フォーム デザイナーで必要です。
+  Private components As System.ComponentModel.IContainer
+
+  'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+  'Windows フォーム デザイナーを使用して変更できます。  
+  'コード エディターを使って変更しないでください。
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.SuspendLayout()
+    '
+    'Label1
+    '
+    Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+    Me.Label1.ForeColor = System.Drawing.Color.White
+    Me.Label1.Location = New System.Drawing.Point(0, 0)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(284, 261)
+    Me.Label1.TabIndex = 0
+    Me.Label1.Text = "Delete" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Enter" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Eecape" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "K"
+    Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.BackColor = System.Drawing.Color.Black
+    Me.ClientSize = New System.Drawing.Size(284, 261)
+    Me.Controls.Add(Me.Label1)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+    Me.Name = "Form1"
+    Me.Opacity = 0.5R
+    Me.Text = "Form1"
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+  Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+  End Sub
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Me.Top = System.Windows.Forms.Screen.FromControl(Me).Bounds.Height - Me.Height
+    Me.Left = System.Windows.Forms.Screen.FromControl(Me).Bounds.Left
+  End Sub
+End Class
